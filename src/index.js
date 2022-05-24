29c13487b62d22e755df095e8dc8b4a35d053f67
Emilio Maoddi <emilio.maoddi@ericsson.com>
2022-05-24 14:37:07 +0200

    second-commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const title = document.querySelector('title');
+title.text = 'Task Tracker'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

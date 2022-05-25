8d7de0cbcb3cdba05f4a77559c13b961fa23ac9d
Emilio Maoddi <emilio.maoddi@ericsson.com>
2022-05-25 15:44:41 +0200

    eighth-commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import Header from "./Header"
+import { Link } from 'react-router-dom'
+
 const About = () => {
   return (
     <div>
-        <h4>version 1.0.0</h4>
-        <a href='/'>Go Back</a>
+        <h4 style={{center: 'true'}}>version 1.0.0</h4>
+        {/* <a href='/'>Go Back</a> */}
+        <Link to='/'>Go Back</Link>
     </div>
   )
 }
